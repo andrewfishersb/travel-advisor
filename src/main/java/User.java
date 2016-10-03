70757712ff87ffc2f8b7c1d26675942c6eebe705
Andrew Fisher and Andrew Merrell <andrew@>
2016-10-03 11:18:26 -0700

    merge two branches adding information from the user

---
+++ src/main/java/User.java
@@ -0,0 +1,31 @@
+public class User {
+  public String name;
+  public String email;
+  public int age;
+  public int id;
+}
+public User(String name, String email, int age){
+  this.name = name;
+  this.email = email;
+  this.age = age;
+  this.id = id;
+}
+
+public String getName(){
+  return name;
+}
+public String getEmail(){
+  return email;
+}
+public int getAge(){
+  return age;
+}
+public static List<User>all(){
+  String sql = "SELECT * FROM users";
+  try(Connection con = DB.sql2o.open()) {
+   return con.createQuery(sql).executeAndFetch(User.class);
+ }
+}
+public int getId() {
+   return id;
+ }
