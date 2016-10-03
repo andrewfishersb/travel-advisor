c0308e9357f3218385d52d1de7663b33d46540ee
Andrew Fisher and Andrew Merrell <andrew@>
2016-10-03 10:50:02 -0700

    create the backend branch with a completed abstract class and the start of the Flight class

--- src/main/java/Booking.java
+++ src/main/java/Booking.java
@@ -1,8 +1,49 @@
-
+import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public abstract class Booking {
-    protected String startDate;
-    protected String endDate;
+  protected String startDate;
+  protected String endDate;
+  protected int price;
+  protected int groupSize;
+  protected int userId;
+  protected int id;
+
+  public String getStartDate() {
+    return startDate;
+  }
+
+  public String getEndDate() {
+    return endDate;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public int getGroupSize() {
+    return groupSize;
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  @Override
+  public boolean equals(Object otherBooking){
+    if(!(otherBooking instance of Booking)){
+      return false;
+    }
+    else{
+      Booking newBooking = (Booking) otherBooking;
+      return this.startDate.equals(newBooking.endDate) && this.endDate.equals(newBooking.endDate) && this.price == newBooking.price && this.userId == newBooking.userId;
+    }
+  }
 
 }
