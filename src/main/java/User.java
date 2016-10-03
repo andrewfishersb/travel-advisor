a8bd27434eb25fc9978776e00653e830a2db2ef3
david bethune <dbethune1970@gmail.com>
2016-10-03 10:13:44 -0700

    user.java started

---
+++ src/main/java/User.java
@@ -0,0 +1,15 @@
+public class User {
+  String name;
+  String email;
+  int age;
+}
+
+public String getName(){
+  return name;
+}
+public String getEmail(){
+  return email;
+}
+public int getAge(){
+  return age;
+}
