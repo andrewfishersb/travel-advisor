baa4aefc04d9f3d96c32dde372ba50259edaa5f3
David Bethune <dbethune1970@gmail.com>
2016-10-04 13:31:27 -0700

    car test added

---
+++ src/test/java/CarTest.java
@@ -0,0 +1,54 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class CarTest{
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void Car_instantiatesCar(){
+    Car aCar = new Car("Today","Tomorrow",500,2,1,"PDX","SBA");
+    assertTrue(aCar instanceof Car);
+  }
+
+  @Test
+  public void Car_savesCarToDataBase_true(){
+    Car aCar = new Car("Today","Tomorrow",500,2,1,"SBA","PDX");
+    aCar.save();
+    System.out.println("Car: "+aCar.getStartLocation());
+
+    System.out.println("Size: "+Car.all().get(0).getStartLocation());
+
+    assertTrue(Car.all().get(0).equals(aCar));
+  }
+
+  @Test
+  public void Car_returnAllCars_true(){
+    Car aCar = new Car("Today","Tomorrow",500,2,1,"PDX","SBA");
+    Car anotherCar = new Car("Today","Tomorrow",55,2234,12,"PDX","SBA");
+    aCar.save();
+    anotherCar.save();
+    assertEquals(2, Car.all().size());
+  }
+
+  @Test
+  public void Car_findCarsFromDataBase_true(){
+    Car aCar = new Car("Today","Tomorrow",500,2,1,"PDX","SBA");
+    aCar.save();
+    assertTrue(Car.find(aCar.getId()).equals(aCar));
+  }
+
+  @Test
+  public void Car_DeletesFromDataBase_true(){
+    Car aCar = new Car("Today","Tomorrow",500,2,1,"PDX","SBA");
+    aCar.save();
+    aCar.delete();
+    assertEquals(0, Car.all().size());
+  }
+}
