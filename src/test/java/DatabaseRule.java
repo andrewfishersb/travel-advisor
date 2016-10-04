505f75838ddbaa6027e5d9e587ceca0367c06071
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-10-04 14:52:28 -0700

    resolve Car.java test issues.

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,7 +5,7 @@ public class DatabaseRule extends ExternalResource {
 
   @Override
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/travel_advisor_test", "postgres", "panthers");
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/travel_advisor_test", null, null);
   }
 
   @Override
